19692c989b05a650d35bcb12c7aa58afc90a6d0d
GEOFFEC1 <elyse.geoffroy@abbvie.com>
2025-05-07 11:08:14 -0500

    add reports test for same report across multiple models

--- inst/tinytest/testCheck.R
+++ inst/tinytest/testCheck.R
@@ -692,7 +692,7 @@ expect_silent_xl(
                                                modelID2 = "https://www.domain.com/report.html"))
 )
 
-export_silent_xl(
+expect_silent_xl(
   addReports(study, reports = list(modelID = "/projects/abv/users/user5xx/Projects/test/Human_analysis_v1.html", modelID2 = "/projects/abv/users/user5xx/Projects/test/Human_analysis_v1.html")),
 )
 # checkResultsLinkouts ---------------------------------------------------------
